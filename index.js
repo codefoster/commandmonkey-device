2fb171b6ba0c0a729dcd850ac3ea9d573fb90676
Jeremy Foster <jeremy.foster@live.com>
2016-05-27 21:48:34 -0700

    convert to johnny-five

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-﻿var cylon = require('cylon');
-var socket = require('socket.io-client')
-    .connect('http://CommandMonkey.azurewebsites.net');
+﻿var five = require('johnny-five');
+var Pi = require("raspi-io");
 
+var board = new five.Board({
+  io: new Pi()
+});
+
+var socket = require('socket.io-client')
+    .connect('http://commandmonkey.azurewebsites.net');
+    
 //work on a real device
-cylon.robot({
-    name: 'edison',
-    connections: { edison: { adaptor: 'intel-iot' } },
-    devices: { monkey: { driver: 'direct-pin', pin: 2 } },
-    work: function (edison) {
-        socket.emit('setTarget');
-        socket.on('command', function (cmd) {
-            edison.monkey.digitalWrite(1);
-            setTimeout(function () { edison.monkey.digitalWrite(0); }, 2000);
-        })
-    }
+board.on("ready", function() {
+    var monkey = new five.Pin("GPIO23");
+    var light = new five.Led("GPIO24");
+    
+    light.on();
+
+    socket.emit('setTarget');
+    socket.on('command', function (cmd) {
+        monkey.high();
+        setTimeout(function () { monkey.low(); }, 2000);
+    })
+    
+    board.on("warn", function(event) {
+        //when the process shuts down, turn off the light
+        if(event.class === 'Board' || event.message === 'Closing.') {
+            monkey.low();
+            light.off();
+        }
+    });
 }).start();
